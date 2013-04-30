1cfe15ff9ac77ecdfc55534f08deabfe97f7d7be
ajames <adamjames_pro@yahoo.com>
2013-04-30 06:45:00 -0700

    Adding Scope
    Prepping CMS for admin area and queries

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,6 @@ class Contact < ActiveRecord::Base
   #set_table_name("if renaming")
   
   scope :order, order("contact_last_name ASC").limit(10)
-  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
+  scope :search, lambda {|query| where(["contact_first_name || contact_last_name || contact_organization LIKE ?", "%#{query}%"])}
   
 end
