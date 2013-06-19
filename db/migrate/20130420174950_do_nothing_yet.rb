8bb73c8fa592f4aa758a27428ac63994363be9cc
ajames <adamjames_pro@yahoo.com>
2013-06-19 00:53:12 -0700

    Update major changes

--- db/migrate/20130420174950_do_nothing_yet.rb
+++
@@ -1,9 +0,0 @@
-class DoNothingYet < ActiveRecord::Migration
-  def up
-  	#instructions for changing database to a new state
-  end
-
-  def down
-  	#instructions for changing database to the previous state
-  end
-end
