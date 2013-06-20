d6c9069a0a9783b5cffe38da50a0ac3fd79a4409
AJ <ajamesforwork@gmail.com>
2013-06-20 09:52:19 -0700

    Testign login

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -20,20 +20,6 @@ class AccountController < ApplicationController
 			redirect_to(:action => 'signin')			
 		end
 	end
-
-	def attempt_signup
-		existing_user = AuthUser.where('email = ?', params[:email])	
-		if !existing_user.blank?
-			flash[:notice] = "An account with that email has already been created."
-			#redirect_to(:action => 'signup')
-			redirect_to(:controller => 'user', :action => 'verify')				
-		else
-			flash[:notice] = "Good."
-			#new_user = User.new
-			#if new user is created then send to verify
-			redirect_to(:controller => 'user', :action => 'verify')				
-		end
-	end		
 	
 	def signin
 		@page_title = "Sign In"
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -24,8 +24,17 @@ class UserController < ApplicationController
 	end
 
 	def attempt_signup
-		verify
-		render('verify')	
+		existing_user = AuthUser.where('email = ?', params[:email])	
+		if !existing_user.blank?
+			flash[:notice] = "An account with that email has already been created."
+			#redirect_to(:action => 'signup')
+			redirect_to(:controller => 'user', :action => 'verify')				
+		else
+			flash[:notice] = "Good."
+			#new_user = User.new
+			#if new user is created then send to verify
+			redirect_to(:controller => 'user', :action => 'verify')				
+		end
 	end	
 
 	def verify
