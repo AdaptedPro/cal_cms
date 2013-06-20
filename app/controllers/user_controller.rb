1ca73d8a264f3c5dc5d90bd37d4460b735b29470
AJ <ajamesforwork@gmail.com>
2013-06-20 15:39:51 -0700

    Mysterious breaks

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -26,7 +26,23 @@ class UserController < ApplicationController
 
 	def attempt_signup
 		@user_data = params
-	
+		existing_user = AuthUser.where('email = ?', params[:email])	
+		if !existing_user.blank?
+			flash[:notice] = "An account with that email has already been created."
+			redirect_to(:controller => 'user', :action => 'verify')				
+		else			
+			# Instantiate a new object using form parameters
+			user = User.new(:first_name => params[:first_name], 
+							:last_name => params[:last_name], 
+							:email => params[:email], 
+							:hashed_password => Digest::SHA1.hexdigest(params[:password]))
+
+			if user.save
+				flash[:notice] = "User created."
+			else
+				flash[:notice] = "User not created."
+			end				
+		end		
 		render('verify')		
 	end	
 
