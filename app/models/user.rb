9a61e74b8520c36b07bdfed30ed97056688aa5bf
AJ <ajamesforwork@gmail.com>
2013-06-20 10:59:14 -0700

    Testign login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ require 'digest/sha1'
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :first_name, :last_name, :email, :hashed_password
-
-    scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
