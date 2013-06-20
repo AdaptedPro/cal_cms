5a6d84d48f672d01e549c86e8e2deabc5f4fef9a
AJ <ajamesforwork@gmail.com>
2013-06-20 10:24:00 -0700

    Testign login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -24,25 +24,8 @@ class UserController < ApplicationController
 	end
 
 	def attempt_signup
-		existing_user = AuthUser.where('email = ?', params[:email])	
-		if !existing_user.blank?
-			flash[:notice] = "An account with that email has already been created."
-			redirect_to(:controller => 'user', :action => 'verify')				
-		else			
-			# Instantiate a new object using form parameters
-			@user = User.new(:first_name => params[:first_name_field], :last_name => params[:last_name_field], :email => params[:email_field],
-				:hashed_password => Digest::SHA1.hexdigest(params[:password]))
-			if @user.save
-				flash[:notice] = "User created."
-				#redirect_to(:action => 'list')
-			else
-				#render('new')
-				flash[:notice] = "User not created."
-				@user_data = params
-			end	
-
-			redirect_to(:controller => 'user', :action => 'verify')				
-		end
+		@user_data = params
+		redirect_to(:controller => 'user', :action => 'verify')
 	end	
 
 	def verify
