f15571792898e61b0ac3bfaa9d8b7dddddc1fd78
AJ <ajamesforwork@gmail.com>
2013-06-21 15:15:07 -0700

    Action Mailer woes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,15 +61,15 @@ CalCms::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@cirrusera.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'mail.adaptedpro.net',
+    port:                 587,
+    domain:               'ce.adaptedpro.net',
+    user_name:            'noreply@ce.adaptedpro.net',
+    password:             'x7n^Y8Jp',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
