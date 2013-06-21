982ccd6e7bea61d596352a0103b2912b7430fa8c
AJ <ajamesforwork@gmail.com>
2013-06-21 13:57:31 -0700

    Update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -45,7 +45,7 @@ class MainController < ApplicationController
 			session[:fb] = true
 			render :json => { :reply => 'good' }
 		else
-			render :json => { :reply => 'bad', :verified => params[:verified] }		
+			render :json => { :reply => 'bad'}		
 		end
 	end
 
