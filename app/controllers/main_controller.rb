3d3ead13ef441dce20ec166afdd684aa60d757b1
AJ <ajamesforwork@gmail.com>
2013-06-21 13:50:29 -0700

    Update JSON

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -38,17 +38,14 @@ class MainController < ApplicationController
 	end	
 
 	def facebook
-		if params[:verified] == true #|| params[:link].start_with?('https://www.facebook.com/')
+		if params[:verified] == true 
+		#|| params[:link].start_with?('https://www.facebook.com/')
 			session[:user_id] = params[:id]
 			session[:email] = params[:email]
 			session[:fb] = true
-			flash[:notice] = "You are now logged in via facebook."
-			#redirect_to(:controller => 'dashboard', :action => 'index')
 			render :json => { :reply => 'good' }
 		else
-			flash[:notice] = "Invalid login."
-			#redirect_to(:action => 'signin')	
-			render :json => { :reply => 'bad' }		
+			render :json => { params }		
 		end
 	end
 
