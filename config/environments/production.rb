c2805ed934e88010b6c75431ac84268e2f94a0a9
ajames <adamjames_pro@yahoo.com>
2013-06-24 18:01:02 -0700

    Bug testing continues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,16 +60,15 @@ CalCms::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'mail.adaptedpro.net',
-    port:                 587,
-    domain:               'ce.adaptedpro.net',
-    user_name:            'noreply@ce.adaptedpro.net',
-    password:             'x7n^Y8Jp',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'baci.lindsaar.net',
+  :user_name            => 'ajamesforwork',
+  :password             => 'Ava&Arik04',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
